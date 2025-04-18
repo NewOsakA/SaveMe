4fd83db1231e95dffc40e93205756c00bdedc54a
NewOsakA <115053961+NewOsakA@users.noreply.github.com>
2025-04-18 09:35:54 +0700

    Initial Commit

---
+++ SaveMe/SaveMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveMeApp.swift
+//  SaveMe
+//
+//  Created by Achirawich Phongpanich on 18/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct SaveMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
